05ddb0b76e124a6392e62b755960a1d564cab74f
Krystian Maniewski <krystian.maniewski@atos.net>
2022-09-15 13:30:07 +0200

    poprawki UI i zmiana komponentow

--- src/Components/GoodWeather.jsx
+++ src/Components/GoodWeather.jsx
@@ -2,35 +2,36 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Cities } from "./Cities";
 
+
 class GoodWeather extends Component {
     constructor(props) {
         super(props)
         this.state = {
             weather: [],
-            miasta: [],
+            changeStateVariable: true
         }
     }
 
     miastaZaznaczone = []
 
-    miasta = Cities;
     getWeather() {
         axios.get('http://localhost:8080/cities?miasta=' + this.miastaZaznaczone)
             .then(Response => {
                 console.log(Response)
-                this.setState({ weather: Response.data })
+                this.setState({
+                    weather: Response.data,
+                    changeStateVariable: false
+                })
             })
-
     }
 
-    DisableIfMoreThan5(value){
-        if(this.miastaZaznaczone.length<5){
+    DisableIfMoreThan5(value) {
+        if (this.miastaZaznaczone.length < 5) {
             this.checkIfChecked(value)
         }
-        else{
+        else {
             document.getElementById(value).checked = false
             delete this.miastaZaznaczone[this.miastaZaznaczone.indexOf(value)]
-          
         }
         this.miastaZaznaczone = this.miastaZaznaczone.filter((a) => a);
         console.log(this.miastaZaznaczone)
@@ -46,31 +47,47 @@ class GoodWeather extends Component {
         this.miastaZaznaczone = this.miastaZaznaczone.filter((a) => a);
     }
 
+    changingToTrue() {
+        this.setState({
+            changeStateVariable: true,
+            weather: []
+        })
+        this.miastaZaznaczone = []
+    }
+
     render() {
         const { weather } = this.state
         return (
-
             <div >
+                <div className="ListOfCheckboxes">
                     {
-                        Cities.map((miasto) =>
+                        this.state.changeStateVariable ? Cities.map((miasto) =>
                             <div key={miasto.name}>
                                 <input type="checkbox" className="checkboxes" id={miasto.name} onClick={() => this.DisableIfMoreThan5(miasto.name)}></input>
                                 <label htmlFor={miasto.name} >{miasto.name}</label>
                             </div>)
+                            : <div>
+                                <button className="Buttons" onClick={() => this.changingToTrue()} >
+                                    Choose Cities
+                                </button>
+                            </div>
                     }
+                </div>
 
-                    <div className="Weather">
-                        {
-                            weather.length ?
-                                weather.map(weather => <div  key={weather.name}>{weather.name} {weather.feels_like}
+                <div className="Weather">
+                    {
+                        weather.length ?
+                            weather.map(weather => <div key={weather.name}>{weather.name} {weather.feels_like}
+                                <img alt="" src={weather.icon}></img>
+                            </div>) : null
 
-                                    <img alt="" src={weather.icon}></img>
-
-                                </div>) : null
-                        }
-                    </div>
-                <button className="SubmitButton" onClick={() => this.getWeather()}>Submit</button>
+                    }
+                </div>
+                {
+                    this.state.changeStateVariable ? <button className="Buttons" onClick={() => this.getWeather()}>Submit</button> : null
+                }
             </div>
+
         )
     }
 
